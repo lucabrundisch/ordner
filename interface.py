b06f1bb46bca53593a6694ca00f8df934a218d57
lucabrundisch <lubr4712@th-wildau.de>
2026-02-11 14:20:42 +0100

    erster Commit

--- interface.py
+++ interface.py
@@ -1,1 +1,8 @@
+#!/usr/bin/env python
+interface="gigabitethernet0/0"
+address="192.168.10.1"
+mask="255.255.255.0"
 
+print("enable\n"+"conf t\n")
+print("interface"+interface)
+print("ip address "+address+""+mask+"\n")
